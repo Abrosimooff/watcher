64795bb25a021bf429522d04773c55ebea6db8fd
victor <abrosimov@vistgroup.ru>
2019-07-31 15:02:17 +0700

    Initial commit

---
+++ mailer.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import smtplib
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from settings import WATCHER_EMAIL, WATCHER_PASSWORD, SMTP_IP, SMTP_PORT
+from jinja2 import Template
+
+
+class Mailer:
+    """ Отпарвка email """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def _send(to_email, message_string):
+        """ Отпарвить сформированное email-сообщение """
+        server = smtplib.SMTP_SSL(SMTP_IP, SMTP_PORT)
+        server.ehlo()
+        server.login(WATCHER_EMAIL, WATCHER_PASSWORD)
+        server.sendmail(WATCHER_EMAIL, to_email, message_string)
+        server.close()
+
+    def send_html(self, to_email, subject, context, template_path):
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = WATCHER_EMAIL
+        msg['To'] = to_email
+
+        html = open(template_path).read()
+        template = Template(html.decode('utf8'))
+        html = template.render(**context)
+
+        part = MIMEText(html.encode('utf8'), 'html')
+        msg.attach(part)
+        self._send(to_email, msg.as_string())
+
+    def send_text(self, to_email, subject, text):
+        subject_base64 = base64.encodestring(subject.encode('utf8')).strip()
+        subject = u" =?UTF-8?B?%s?=" % subject_base64
+        message = u"""From: %s\nTo: %s\nSubject: %s\n\n%s
+        """ % (WATCHER_EMAIL, u", ".join([to_email]), subject, text)
+        self._send(to_email, message.encode('utf8'))
---
+++ watcher.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+import datetime
+import json
+import os
+
+import time
+import urllib
+import requests
+
+from django.utils.functional import cached_property
+
+from mailer import Mailer
+
+HOME_PATH = os.path.join(os.path.expanduser("~"), "work", "watcher")
+
+
+class Doctor:
+    auth_data = {
+        u"id_hospital": u"bece99bb-9d2a-49f9-bded-8c9b56acc773",
+        u"surname": u"Абросимова",
+        u"name": u"Арина",
+        u"birthday": u"2019-04-29",
+        u"insurance_policy": 4295089770000080,
+        u"passport": None,
+        u"phone": u"",
+    }
+    DOCTOR_LIST_PATH = os.path.join(HOME_PATH, 'doctor_list.json')
+    WATCH_LIST_PATH = os.path.join(HOME_PATH, 'watch_list.json')
+    from_email = u'abrosimooff@yandex.ru'
+    from_pswd = u'd343h3k0'
+    session = None
+    session_data = {}
+    authorized = False
+
+    def __init__(self):
+        self.session = requests.session()
+        self.session.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+            'Content-Type': 'application/json',
+            'Host': 'vrach42.ru',
+            'Origin': 'https://vrach42.ru',
+            'Referer': 'https://vrach42.ru/login',
+        })
+
+    @property
+    def watch_doctor_list(self):
+        """ Данные о врачах, которые нам важны """
+        doctor_ids = map(str, self.get_watch_doctor_list())
+        if doctor_ids:
+            watch_doctor_list = filter(lambda x: x['id_doctor'] in doctor_ids, self.doctor_list)
+            for doctor in watch_doctor_list:
+                doctor['tickets'] = self.get_tickets(doctor)
+                doctor['opened_tickets'] = {}  # Открытые талоны для записи
+                for ticket in doctor['tickets']:
+                    opened_list = filter(lambda x: not x['is_closed'], ticket['timeSlots'])  # открытое для записи
+                    if opened_list:
+                        doctor['opened_tickets'][ticket['value']] = opened_list
+            return watch_doctor_list
+        return []
+
+    def get_watch_doctor_list(self):
+        """ список докторов, за которыми следим """
+        if os.path.exists(self.WATCH_LIST_PATH):
+            with open(self.WATCH_LIST_PATH, "r") as read_file:
+                watch = json.load(read_file)
+                return watch['id_doctor']
+
+    def get_tickets(self, doctor_item):
+        """ Получить записи к врачу"""
+        if not doctor_item['tickets_cnt']:
+            return []
+        if not self.authorized:
+            self.authorize()
+
+        start = datetime.date.today()
+        end = start + datetime.timedelta(days=90)
+        doctor_params = {
+            'id_hospital': 'bece99bb-9d2a-49f9-bded-8c9b56acc773',
+            'id_hospital_type': '1',
+            'id_doctor': doctor_item['id_doctor'],
+            'id_specialty': doctor_item['id_specialty'],
+            'begin_date': start.strftime('%Y-%m-%d'),
+            'end_date': end.strftime('%Y-%m-%d')
+        }
+        url = 'https://vrach42.ru/v1/tickets?' + urllib.urlencode(doctor_params, doseq=True)
+        response = self.session.get(url)
+        return response.json()
+
+    def authorize(self):
+        """ авторизоваться """
+        if 'session_id' in self.session_data:
+            return
+
+        response = self.session.post('https://vrach42.ru/v1/patients/state/check', json=self.auth_data)  # авторизация
+        response = response.json()
+        if 'session_id' in response:
+            self.session_data = response  # lname, fname, area, session_id
+            self.session.headers.update({'X-Patient-Session': self.session_data['session_id']})
+            self.authorized = True
+            print u'Успешно авторизованы'
+            return True
+        else:
+            self.session_data = response  # ['details']['message']
+            self.authorized = False
+            return False
+
+    def is_actual(self, on_time):
+        """ Актуальна ли информация на выбранное время """
+        return on_time > (datetime.datetime.now() - datetime.timedelta(hours=3))
+
+    @cached_property
+    def doctor_list(self):
+        """ получить список врачей """
+
+        # Если есть инфа в файле и она актуальна, то берём оттуда
+        if os.path.exists(self.DOCTOR_LIST_PATH):
+            timestamp = os.path.getmtime(self.DOCTOR_LIST_PATH)
+            create_time = datetime.datetime.fromtimestamp(timestamp)
+            if self.is_actual(create_time):
+                with open(self.DOCTOR_LIST_PATH, "r") as read_file:
+                    return json.load(read_file)
+
+        # Берём с сайта данные
+        self.authorize()
+        if self.authorized:
+            response = self.session.get('https://vrach42.ru/v1/doctors')
+            doctor_list = response.json()
+            if isinstance(doctor_list, list):
+                self.save_doctor_list(doctor_list)
+                return doctor_list
+        else:
+            print u'Warning: Мы не авторизовались и список врачей пуст'
+        return []
+
+    def save_doctor_list(self, doctor_list):
+        if doctor_list:
+            with open(self.DOCTOR_LIST_PATH, 'w') as f:
+                json.dump(doctor_list, f)
+            print u'Список врачей сохранён'
+
+    def send_doctor_list_to_email(self, doctor_list):
+        """ Отправить весь список докторов на почту """
+        if doctor_list:
+            context = dict(doctor_list=doctor_list)
+            Mailer().send_html(u'abrosimooff@gmail.com', u'Обстановка на "Врач42"', context, 'template.jinja2')
+
+    def one_loop(self):
+        """ Один цикл работы программы """
+        self.send_doctor_list_to_email(self.watch_doctor_list)
+        print 'ok.'
+
+    def loop(self, hours=3):
+        """ Запустить цикл каждые hours часов """
+        seconds = 60 * 60 * hours  # часы в секунды
+        while True:
+            self.one_loop()
+            time.sleep(seconds)  # in seconds
+
+
+doc = Doctor()
+doc.one_loop()
+# doc.loop(hours=3)
